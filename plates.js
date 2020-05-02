42c8a199ae6323e08eb6d5ebd81e3b57b5a1957a
Richard Audette <richard@hotelexistence.ca>
2020-05-02 13:42:38 -0400

    - settled on Pi Camera mode 6, removed other options

--- plates.js
+++ plates.js
@@ -1,64 +1,18 @@
-const timestamp = require('time-stamp');
-var openalpr = require ("node-openalpr");
 var express = require('express')
 var app = express()
 var sqlite3 = require('sqlite3').verbose()
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 
-const { StillCamera, StreamCamera, Codec, SensorMode } = require("pi-camera-connect");
+const { StillCamera, StreamCamera, Codec, SensorMode } = require('pi-camera-connect');
 
 var imageid =0;
 
-//this will turn the capture on or off
 var bCaptureOn=false;
-//Stream or Still mode
-var bStreamMode=false;
-//Stream Sensor Mode
-var iStreamSensorMode=0;
-
-//get cameras started
-const stillCamera = new StillCamera();
-const streamCamera0 = new StreamCamera({
-	codec: Codec.MJPEG,
-	sensorMode: SensorMode.AutoSelect
-	});
-const streamCamera1 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 30,
-	sensorMode: SensorMode.Mode1
-	});
-const streamCamera2 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 15,
-	sensorMode: SensorMode.Mode2
-	});
-const streamCamera3 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 2,
-	sensorMode: SensorMode.Mode3
-	});
-const streamCamera4 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 42,
-	sensorMode: SensorMode.Mode4
-	});
-const streamCamera5 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 49,
-	sensorMode: SensorMode.Mode5
-	});
-const streamCamera6 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 60,
-	sensorMode: SensorMode.Mode6
-	});
-const streamCamera7 = new StreamCamera({
-	codec: Codec.MJPEG,
-	fps: 90,
-	sensorMode: SensorMode.Mode7
-	});
-var streamCamera = streamCamera0;
 
 var db = new sqlite3.Database('plates.db');
+
+var identifyprocess;
 
 var server = app.listen(3000, function () {
 
@@ -74,50 +28,28 @@ app.use('/images', express.static(__dirname + '/images'));
 app.get('/togglecapture', function(req, res){
 	if (bCaptureOn) {
 		bCaptureOn=false;
+		identifyprocess.kill('SIGINT')
 	}
 	else {
 		bCaptureOn=true;
+		//identifyprocess = spawn('node /home/pi/plates/onlyidentify.js', ['interval=5']);
+		identifyprocess = spawn('node',['/home/pi/plates/onlyidentify.js','interval=5'], {stdio: 'ignore' });
 	}
 	res.redirect('/');
 });
 
-app.get('/togglestreammode', function(req, res){
-	if (bStreamMode) {
-		bStreamMode=false;
-		streamCamera.stopCapture();
-	}
-	else {
-		bStreamMode=true;
-		streamCamera.startCapture();
-	}
-	res.redirect('/');
+app.get('/poweroff', function(req, res){
+
+	shutdown(function(output){
+		console.log(output);
+	});
+
+	res.send("Shutting down");
 });
 
-function setMode() {
-	if (bStreamMode) {	streamCamera.stopCapture();}
-	if (iStreamSensorMode==0) {streamCamera = streamCamera0;}
-	if (iStreamSensorMode==1) {streamCamera = streamCamera1;}
-	if (iStreamSensorMode==2) {streamCamera = streamCamera2;}
-	if (iStreamSensorMode==3) {streamCamera = streamCamera3;}
-	if (iStreamSensorMode==4) {streamCamera = streamCamera4;}
-	if (iStreamSensorMode==5) {streamCamera = streamCamera5;}
-	if (iStreamSensorMode==6) {streamCamera = streamCamera6;}
-	if (iStreamSensorMode==7) {streamCamera = streamCamera7;}
-	if (bStreamMode) {	streamCamera.startCapture();}
+function shutdown(callback){
+    exec('sudo shutdown now', function(error, stdout, stderr){ callback(stdout); });
 }
-
-app.get('/setsensormode', function(req, res){
-	if (typeof req.query.mode !== 'undefined') {
-		var iMode = parseInt(req.query.mode);
-		if (Number.isInteger(iMode)) {
-			if (iMode>7) {iStreamSensorMode=0;}
-			else if (iMode<0) {iStreamSensorMode=0;}
-			else {iStreamSensorMode=iMode;}
-			setMode(); 
-			}
-	}
-	res.redirect('/');
-});
 
 app.get('/', function(req, res){	
 
@@ -167,25 +99,12 @@ app.get('/', function(req, res){
 	strQuery= "select * from plates " + whereclause + "order by id desc " + limit;
 	console.log(strQuery);
 
-	var sStreamSensorMode="";
-	if (iStreamSensorMode==0) {sStreamSensorMode = "Auto";}
-	if (iStreamSensorMode==1) {sStreamSensorMode = "1: 1920x1080 30 fps";}
-	if (iStreamSensorMode==2) {sStreamSensorMode = "2: 2592x1944 15 fps";}
-	if (iStreamSensorMode==3) {sStreamSensorMode = "3: 2592x1944 2 fps";}
-	if (iStreamSensorMode==4) {sStreamSensorMode = "4: 1296x972 42 fps";}
-	if (iStreamSensorMode==5) {sStreamSensorMode = "5: 1296x730 49 fps";}
-	if (iStreamSensorMode==6) {sStreamSensorMode = "6: 640x480 60 fps";}
-	if (iStreamSensorMode==7) {sStreamSensorMode = "7: 640x480 90 fps";}
-
-
 	db.all(strQuery, function(err,rows) {
 		res.render('plates.ejs', {
 			bCaptureOn: bCaptureOn,
 			plates: rows,
 			topid: topid,
 			bottomid: bottomid,
-			sStreamSensorMode: sStreamSensorMode,
-			bStreamMode: bStreamMode,
 			includenomatch: includenomatch
 		});
 	});
@@ -193,82 +112,3 @@ app.get('/', function(req, res){
 
 	});	
 
-function ShootAndAssess () {
-
-	if (bCaptureOn) {
-		var filename = './images/img'+timestamp('YYYYMMDDHHmmss')+'.jpg';
-
-		if (bStreamMode) {
-			startTime = new Date();
-			streamCamera.takeImage().then(image => {
-				stopTime = new Date();
-				console.log('stream image taken set:'+ (stopTime-startTime));
-				fs.writeFileSync(filename, image);
-				identify(imageid, filename);
-				imageid = imageid +1;
-			});
-		}
-		else {
-			startTime = new Date();
-			stillCamera.takeImage().then(image => {
-				stopTime = new Date();
-				console.log('still image taken set:'+ (stopTime-startTime));
-				fs.writeFileSync(filename, image);
-				identify(imageid, filename);
-				imageid = imageid +1;
-				});
-		}
-/* works
-		const stillCamera = new StillCamera();
-		stillCamera.takeImage().then(image => {
-			fs.writeFileSync(filename, image);
-			identify(imageid, filename);
-			imageid = imageid +1;
-			});
-*/
-/*			const streamCamera = new StreamCamera({
-				codec: Codec.MJPEG
-				});
-			console.log('codec set')
-			await streamCamera.startCapture();
-			streamCamera.takeImage().then(image => {
-				console.log('stream image taken set')
-				fs.writeFileSync(filename, image);
-				identify(imageid, filename);
-				imageid = imageid +1;
-				await streamCamera.stopCapture();
-			});
-*/
-
-	}
-}
-
-function identify (id, path) {
-	console.log (openalpr.IdentifyLicense (path, function (error, output) {
-		var results = output.results;
-		console.log (id +" "+ output.processing_time_ms +" "+ ((results.length > 0) ? results[0].plate : "No results"));
-		console.log(results);
-		var currentplate = 0;
-		if (results.length == 0) {
-			query = "insert into plates ( filename, processtime, plate, confidence,streammode,sensormode) values ('"+path+"','','','0',"+bStreamMode+","+iStreamSensorMode+");";
-			console.log(query);
-			db.run(query);
-			}
-		while ((results.length > 0) && (currentplate<results.length)) {
-			query = "insert into plates ( filename, processtime, plate, confidence,streammode,sensormode) values ('"+path+"','"+ output.processing_time_ms+"','"+results[currentplate].plate+"','"+results[currentplate].confidence+"',"+bStreamMode+","+iStreamSensorMode+");";
-			console.log(query);
-			db.run(query);
-			currentplate++;
-		}
-	
-	}));
-}
-
-var a;
-a = openalpr.Start ();
-console.log(a);
-a=openalpr.GetVersion ();
-console.log(a);
-
-//identify (0, "ea7the.jpg");
-setInterval(ShootAndAssess, 15*1000);
