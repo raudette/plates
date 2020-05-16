4efb43503d0c75539b7fa787c36d3ebd01c24633
Richard <richard_audette@yahoo.ca>
2020-05-16 10:47:06 -0400

    - moved picture taking back into main, moved ALPR processing out, prep for adding GPS

--- plates.js
+++ plates.js
@@ -1,10 +1,97 @@
 var express = require('express')
 var app = express()
 var sqlite3 = require('sqlite3').verbose()
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
+const timestamp = require('time-stamp');
+var fs = require('fs');
+
+var exec = require('child_process').exec; //we use this EXEC to shut down
+//we use this child to run our ALPR process
+const cp = require('child_process');
+const alpr = cp.fork('alpr.js');
+
+alpr.on('message', (m) => {
+	console.log('PARENT got message:', m);
+	if (bCaptureOn) {
+		shoot();
+	}
+  });
+
+//use websockets to poll GPS, push stuff to client
+var WebSocketServer = require('ws').Server;
+
+wss = new WebSocketServer({port: 8080})
+CLIENTS=[];
+
+wss.on('connection', function(ws) {
+	CLIENTS.push(ws);
+	console.log("connected");
+});
+
+function wsSendAll (message) {
+    for (var i=0; i<CLIENTS.length; i++) {
+        CLIENTS[i].send(message);
+    }
+}
+
+app.post('/plates/:id/addgps', function (req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+		author = fields.author;
+		title = fields.title;
+		pool.getConnection(function(err, connection) {
+			strQuery = "INSERT INTO readinglog.readinglog (`userid`, `author`, `title`, `date`) VALUES ("+req.params.id+", '"+author+"', '"+title+"',concat(year(now()),'-',month(now()),'-',day(now()) ) );";
+			console.log(strQuery);
+			connection.query( strQuery, function(err, rows) {
+				connection.release();
+				});
+			});
+		res.sendStatus(200);
+	    });
+
+});
 
 const { StillCamera, StreamCamera, Codec, SensorMode } = require('pi-camera-connect');
+
+//node: modified still camera code to force sports exposure
+const stillCamera = new StillCamera();
+
+//5	1640x922	16:9	1/10 <= fps <= 40	x	 	Full	2x2
+//6	1280x720	16:9	40 < fps <= 90	x	 	Partial	2x2
+//7	640x480	4:3	40 < fps <= 90	x	 	Partial	2x2
+const streamCamera = new StreamCamera({
+	codec: Codec.MJPEG,
+	fps: 30,
+	sensorMode: SensorMode.Mode5
+	});
+
+const shoot = async () => {
+
+	var filename = './images/img'+timestamp('YYYYMMDDHHmmss')+'.jpg';
+	startTime = new Date();
+		
+	await streamCamera.takeImage().then(image => {
+		stopTime = new Date();
+		console.log(filename);
+		fs.writeFileSync(filename, image);
+		query = "insert into plates ( filename,streammode,sensormode) values ('"+filename+"','1','6');";
+		console.log(query);
+		db.run(query, function(err) {
+			if (err) {
+				console.log("is error here")
+			  return console.log(err.message);
+			}
+			// get the last insert id
+			console.log(`A row has been inserted with rowid ${this.lastID}`);
+			alpr.send({ id: this.lastID });
+			wsSendAll(this.lastID);
+		//run post process here	
+		//identify(imageid, filename);
+		});
+	//    await streamCamera.stopCapture();
+		
+	
+	});
+}
 
 var imageid =0;
 
@@ -25,18 +112,30 @@ var server = app.listen(3000, function () {
 
 app.use('/images', express.static(__dirname + '/images'));
 
-app.get('/togglecapture', function(req, res){
-	if (bCaptureOn) {
-		bCaptureOn=false;
-		identifyprocess.kill('SIGINT')
+app.get('/togglestreammode', function(req, res){
+	if (bStreamMode) {
+		bStreamMode=false;
 	}
 	else {
-		bCaptureOn=true;
-		//identifyprocess = spawn('node /home/pi/plates/onlyidentify.js', ['interval=5']);
-		identifyprocess = spawn('node',['/home/pi/plates/onlyidentify.js','interval=5'], {stdio: 'ignore' });
+		bStreamMode=true;
 	}
 	res.redirect('/');
 });
+
+app.get('/togglecapture', function(req, res){
+	if (bCaptureOn) {
+		bCaptureOn=false;
+		streamCamera.stopCapture();
+	}
+	else {
+		bCaptureOn=true;
+		streamCamera.startCapture();
+		setTimeout(shoot, 3000)
+	}
+	res.redirect('/');
+});
+
+
 
 app.get('/poweroff', function(req, res){
 
---
+++ testalpr.js
@@ -0,0 +1,11 @@
+const cp = require('child_process');
+const n = cp.fork('alpr.js');
+
+n.on('message', (m) => {
+  console.log('PARENT got message:', m);
+  if (m.id==1721) {n.send({ id: 1722 });}
+  if (m.id==1722) {n.send({ id: 1723 });}
+
+});
+
+n.send({ id: 1721 });
